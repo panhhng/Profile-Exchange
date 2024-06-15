0a000ef8b1ff1d6cf1d6bfffccbbaab2a69c0fe9
Anh Nguyen <87385817+panhhng@users.noreply.github.com>
2024-06-15 23:13:47 +0700

    Add files via upload

--- Profile-Exchange-main/profile_exchange/profile_exchangeApp.swift
+++ Profile-Exchange-main/profile_exchange/profile_exchangeApp.swift
@@ -1,17 +1,58 @@
 import UIKit
-import FirebaseCore
-
+import Firebase
+import GoogleSignIn
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
-  var window: UIWindow?
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
 
-  func application(_ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions:
-                   [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    FirebaseApp.configure()
-
-    return true
-  }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance.handle(url)
+    }
 }
+
+class SignInViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the configuration
+        let clientID = FirebaseApp.app()?.options.clientID
+        GIDSignIn.sharedInstance.configuration = GIDConfiguration(clientID: clientID!)
+        
+        // Add a Google Sign-In button
+        let signInButton = GIDSignInButton()
+        signInButton.center = view.center
+        view.addSubview(signInButton)
+    }
+    
+    @IBAction func signInWithGoogle(_ sender: Any) {
+        GIDSignIn.sharedInstance.signIn(withPresenting: self) { signInResult, error in
+            if let error = error {
+                print("Error during sign-in: \(error.localizedDescription)")
+                return
+            }
+            
+            guard let signInResult = signInResult else { return }
+            guard let idToken = signInResult.user.idToken?.tokenString else { return }
+            let accessToken = signInResult.user.accessToken.tokenString
+            
+            let credential = GoogleAuthProvider.credential(withIDToken: idToken, accessToken: accessToken)
+            Auth.auth().signIn(with: credential) { authResult, error in
+                if let error = error {
+                    print("Error during Firebase sign-in: \(error.localizedDescription)")
+                    return
+                }
+                // User is signed in
+                print("User signed in with Google: \(String(describing: authResult?.user.displayName))")
+            }
+        }
+    }
+}
+
+
