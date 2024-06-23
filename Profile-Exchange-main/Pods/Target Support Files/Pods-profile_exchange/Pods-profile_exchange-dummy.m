6842aba642b72ba2456b5314339b0828ec3853de
linhphan03 <phanthuylinh221203@gmail.com>
2024-06-23 02:48:14 -0700

    Remove pods

--- Profile-Exchange-main/Pods/Target Support Files/Pods-profile_exchange/Pods-profile_exchange-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_profile_exchange : NSObject
-@end
-@implementation PodsDummy_Pods_profile_exchange
-@end
--- Profile-Exchange-main/Pods/Target Support Files/Pods-profile_exchangeTests/Pods-profile_exchangeTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_profile_exchangeTests : NSObject
-@end
-@implementation PodsDummy_Pods_profile_exchangeTests
-@end
