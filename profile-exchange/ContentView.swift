65da842db24142d564d4a1c39beaac40a2218718
nigelnh <121311174+nigelnh@users.noreply.github.com>
2024-06-26 17:57:53 +0700

    Initial Commit

---
+++ profile-exchange/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  profile-exchange
+//
+//  Created by Nhân Nguyễn on 6/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ profile-exchange/profile_exchangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  profile_exchangeApp.swift
+//  profile-exchange
+//
+//  Created by Nhân Nguyễn on 6/26/24.
+//
+
+import SwiftUI
+
+@main
+struct profile_exchangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
